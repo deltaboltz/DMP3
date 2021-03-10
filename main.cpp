f5aef06b3897a1b4b58b7a78a4691435787ab9b1
Connor Schultz <deltaboltz@me.com>
2021-03-09 19:37:30 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,45 @@
 #include "train.h"
 #include "data.h"
 
-int main (int argc, char *argv[]) {
+int main (int argc, char *argv[]) 
+{
 	std::string sentence;
 	int i;
 	char c;
 	Data d(104);
 
 	for (i = 0; i < 52; i++)
-  {
+  	{
 		d.train(cs_yes[i], 1);
 		d.train(cs_no[i], 0);
 	}
+	
+	for (i = 1; i < argc; i++) 
+	{
+		sentence += argv[i];
+		sentence += " ";
+	}
+	
+	i = 0;
+	while (sentence[i]) 
+	{
+		sentence[i] = toupper(sentence[i]);
+		i++;
+	}
 
 	for (i = 0; i < 52; i++)
-  {
+  	{
 		Map m = d.vectorize(cs_no[i]);
-    
+    		
+		m.print();
+		
 		double yes = d.naivebayes_num(m, 1);
 		double no = d.naivebayes_num(m, 0);
 		double both = yes + no;
 
 		std::cout << cs_yes[i] << "\n";
+		std::cout << "    CS: " << no << "\t(" << (yes / both)*100 << "%\n";
+		std::cout << "NOT CS: " << no << "\t(" << (yes / both)*100 << "%\n";
 		std::cout << "Class:  " << ((yes > no) ? "    CS\n" : "NOT CS\n\n");
 	}
 }
