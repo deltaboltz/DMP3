3338f159f4f8cd5656b29263c9ce4d265b72e1db
Connor Schultz <deltaboltz@me.com>
2021-03-09 20:46:21 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ int main (int argc, char *argv[])
 	for (i = 0; i < 52; i++) 
 	{
 		Map m = d.vectorize(cs_no[i]);
-		outputFile << m
+		outputFile << m;
 		double yes = d.naivebayes_num(m, 1);
 		double no = d.naivebayes_num(m, 0);
 		double both = yes + no;
@@ -53,7 +53,7 @@ int main (int argc, char *argv[])
 	for(i = 0; i < 52; i++)
 	{
 		Map m = d.vectorize(cs_no[i]);
-		outputFile << m
+		outputFile << m;
 		double yes = d.naivebayes_num(m, 1);
 		double no = d.naivebayes_num(m, 0);
 		double both = yes + no;
