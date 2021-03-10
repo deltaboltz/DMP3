a9cdd3025031f93fa6da778617a85fba859c7245
Connor Schultz <deltaboltz@me.com>
2021-03-09 20:23:42 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,23 @@
 #include <ctype.h>
 #include <string>
 #include <iostream>
-#include <fstream>
 #include "map.h"
-#include "train.h"
+#include "training.h"
 #include "data.h"
 
 int main (int argc, char *argv[]) 
 {
-	std::fstream outputFile("output.txt");
 	std::string sentence;
 	int i;
 	char c;
 	Data d(104);
 
-	for (i = 0; i < 52; i++)
-  	{
+	for (i = 0; i < 52; i++) 
+	{
 		d.train(cs_yes[i], 1);
 		d.train(cs_no[i], 0);
 	}
-	
+
 	for (i = 1; i < argc; i++) 
 	{
 		sentence += argv[i];
@@ -34,20 +32,17 @@ int main (int argc, char *argv[])
 		i++;
 	}
 
-	for (i = 0; i < 52; i++)
-  	{
+	for (i = 0; i < 52; i++) 
+	{
 		Map m = d.vectorize(cs_no[i]);
-    		
-		m.print();
-		
+		//m.print();
 		double yes = d.naivebayes_num(m, 1);
 		double no = d.naivebayes_num(m, 0);
 		double both = yes + no;
 
-		outputFile << cs_yes[i] << "\n";
-		outputFile << "    CS: " << no << "\t(" << (yes / both)*100 << "%\n";
-		outputFile << "NOT CS: " << no << "\t(" << (yes / both)*100 << "%\n";
-		outputFile << "Class:  " << ((yes > no) ? "    CS\n" : "NOT CS\n\n");
+		std::cout << cs_yes[i] << "\n";
+		std::cout << "    CS: " << yes << "\t(" << (yes / both)*100 << " %)\n";
+		std::cout << "NOT CS: " << no << "\t(" << (no / both)*100 << " %)\n\n";
+		std::cout << "Class:  " << ((yes > no) ? "    CS\n" : "NOT CS\n\n");
 	}
-	outputFile.close()
 }
